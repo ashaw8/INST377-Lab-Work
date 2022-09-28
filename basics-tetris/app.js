584ebe4fd422a7ebe28f82e7bcbb09d48b89228c
ashaw8 <104944551+ashaw8@users.noreply.github.com>
2022-09-28 16:04:25 -0400

    Switch location of files

---
+++ basics-tetris/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#Score')
+    const StartBtn = document.querySelector('')
+    const width = 10
+
+
+    
+})
